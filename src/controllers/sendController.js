53a7fade29544c17680a7ba23954a8f6b068da18
Jamie Hewitt <jamie.hewitt15@gmail.com>
2021-09-29 19:36:57 +0300

    Refactoring isAllowed

--- src/controllers/sendController.js
+++ src/controllers/sendController.js
@@ -1,112 +1,85 @@
 const url = require('url')
 const { web3, account } = require('../utils/getWeb3')
-const { getOceanBalance, getEthBalance } = require('../utils/getBalance')
 const { getTokenInstance } = require('../utils/getTokenInstance')
-const { isAllowed } = require('../utils/getIsAllowed')
+const { isAllowed } = require('../utils/isAllowed')
+const { isCoolingDown } = require('../utils/isCoolingDown')
 const { insert, find } = require('../db')
 const { renderHome } = require('./renderHome')
 
 const sendController = async (res, req) => {
   try {
-    let ipAddress =
-      req.headers['x-forwarded-for'] || req.connection.remoteAddress
-    console.log(`ip address - `, ipAddress)
-    const url_parts = url.parse(req.url, true)
-    const { query } = url_parts
-
-    const from = account
-    const to = query.address
-    const value = web3.utils.toWei(process.env.TOKEN_AMOUNT, 'ether')
-
-    // Check if its valid ETH address
-    if (web3.utils.isAddress(to)) {
-      const oceanBalance = await getOceanBalance(to)
-      const EthBalance = await getEthBalance(to)
-      if (
-        process.env.TOKEN_CONTRACT_ADDRESS ===
-          '0x0000000000000000000000000000000000000000' &&
-        EthBalance > process.env.ETH_BALANCE_LIMIT
-      ) {
-        // handle Ether Balance is more than limit
-        renderHome(
-          res,
-          `You already have ${process.env.BASE_TOKEN_NAME} in your wallet.\nPlease come back when you require ${process.env.BASE_TOKEN_NAME}`,
-          false
-        )
-      } else if (
-        process.env.TOKEN_CONTRACT_ADDRESS !==
-          '0x0000000000000000000000000000000000000000' &&
-        oceanBalance > process.env.OCEAN_BALANCE_LIMIT
-      ) {
-        // handle Ocean Balance is more than limit
-        renderHome(
-          res,
-          `You already have Ocean in your wallet.\nPlease come back when you require Ocean`,
-          false
-        )
-      } else {
-        //check if this user is in cool down period
-        await find(query.address, async (records) => {
-          console.log(records[0])
-          if (records[0] && !isAllowed(records[0].lastUpdatedOn)) {
-            renderHome(
-              res,
-              'You have to wait 24 hours between faucet requests',
-              false
-            )
-          } else {
-            //insert ip address into db
-            await insert(
-              { ip: ipAddress, wallet: to, lastUpdatedOn: Date.now() },
-              (result) => console.log(result)
-            )
-
-            if (
-              process.env.TOKEN_CONTRACT_ADDRESS !=
-              '0x0000000000000000000000000000000000000000'
-            ) {
-              //create token instance from abi and contract address
-              const tokenInst = getTokenInstance()
-              tokenInst.methods
-                .transfer(to, value)
-                .send({ from }, async function (error, txHash) {
-                  if (!error) {
-                    console.log('txHash - ', txHash)
-                    renderHome(
-                      res,
-                      `Great!! test OCEANs are on the way !!`,
-                      true,
-                      txHash
-                    )
-                  } else {
-                    console.error(error)
-                  }
-                })
-            } else {
-              // sending ETH
-              web3.eth.sendTransaction(
-                { from, to, value },
-                async function (error, txHash) {
-                  if (!error) {
-                    console.log('txHash - ', txHash)
-                    renderHome(
-                      res,
-                      `Great!! Network funds are on the way !!`,
-                      true,
-                      txHash
-                    )
-                  } else {
-                    console.error(error)
-                  }
-                }
-              )
-            }
-          }
-        })
-      }
+    const { message, status } = await isAllowed(req)
+    if (status === false) {
+      renderHome(res, message, status)
     } else {
-      //handle incorrect address response
-      renderHome(res, `Please enter valid Ethereum Wallet Address`, false)
+      const ipAddress =
+        req.headers['x-forwarded-for'] || req.connection.remoteAddress
+      console.log(`ip address - `, ipAddress)
+      const url_parts = url.parse(req.url, true)
+      const { query } = url_parts
+
+      const from = account
+      const to = query.address
+      const value = web3.utils.toWei(process.env.TOKEN_AMOUNT, 'ether')
+
+      //check if this user is in cool down period
+      await find(query.address, async (records) => {
+        console.log(records[0])
+        if (records[0] && !isCoolingDown(records[0].lastUpdatedOn)) {
+          renderHome(
+            res,
+            'You have to wait 24 hours between faucet requests',
+            false
+          )
+        } else {
+          //insert ip address into db
+          await insert(
+            { ip: ipAddress, wallet: to, lastUpdatedOn: Date.now() },
+            (result) => console.log(result)
+          )
+
+          if (
+            process.env.TOKEN_CONTRACT_ADDRESS !=
+            '0x0000000000000000000000000000000000000000'
+          ) {
+            //create token instance from abi and contract address
+            const tokenInst = getTokenInstance()
+            tokenInst.methods
+              .transfer(to, value)
+              .send({ from }, async function (error, txHash) {
+                if (!error) {
+                  console.log('txHash - ', txHash)
+                  renderHome(
+                    res,
+                    `Great!! test OCEANs are on the way !!`,
+                    true,
+                    txHash
+                  )
+                } else {
+                  console.error(error)
+                }
+              })
+          } else {
+            // sending ETH
+            web3.eth.sendTransaction(
+              { from, to, value },
+              async function (error, txHash) {
+                if (!error) {
+                  console.log('txHash - ', txHash)
+                  renderHome(
+                    res,
+                    `Great!! Network funds are on the way !!`,
+                    true,
+                    txHash
+                  )
+                } else {
+                  console.error(error)
+                }
+              }
+            )
+          }
+        }
+      })
     }
   } catch (err) {
     console.error(err)
---
+++ src/utils/isAllowed.js
@@ -0,0 +1,39 @@
+const url = require('url')
+const { web3 } = require('../utils/getWeb3')
+const { getOceanBalance, getEthBalance } = require('../utils/getBalance')
+
+const isAllowed = async (req) => {
+  let message = null
+  let status = false
+  const url_parts = url.parse(req.url, true)
+  const { query } = url_parts
+  const to = query.address
+  // Check if its valid ETH address
+  if (!web3.utils.isAddress(to)) {
+    message = `Please enter valid Ethereum Wallet Address`
+    return { message, status }
+  }
+  const oceanBalance = await getOceanBalance(to)
+  const EthBalance = await getEthBalance(to)
+
+  if (
+    // Check Ether balance is below limit
+    process.env.TOKEN_CONTRACT_ADDRESS ===
+      '0x0000000000000000000000000000000000000000' &&
+    EthBalance > process.env.ETH_BALANCE_LIMIT
+  ) {
+    message = `You already have ${process.env.BASE_TOKEN_NAME} in your wallet.\nPlease come back when you require ${process.env.BASE_TOKEN_NAME}`
+  } else if (
+    // Check Ether balance is below limit
+    process.env.TOKEN_CONTRACT_ADDRESS !==
+      '0x0000000000000000000000000000000000000000' &&
+    oceanBalance > process.env.OCEAN_BALANCE_LIMIT
+  ) {
+    message = `You already have Ocean in your wallet.\nPlease come back when you require Ocean`
+  } else {
+    status = true
+  }
+  return { message, status }
+}
+
+module.exports = { isAllowed }
